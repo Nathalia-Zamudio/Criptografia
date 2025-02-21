b1d59aed5325c156ae28d9d315f90155932e663e
Nathalia-Zamudio <72407686+Nathalia-Zamudio@users.noreply.github.com>
2025-02-20 20:37:04 -0600

    Update cifrado_Cesar_Tarea1.py

--- Tarea1-Cifrado César/cifrado_Cesar_Tarea1.py
+++ Tarea1-Cifrado César/cifrado_Cesar_Tarea1.py
@@ -1,19 +1,25 @@
 A=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
 diccionario = {A[i]: i for i in range(len(A))}
-frase=input("Ingrese la frase a cifrar:_")
-frase=frase.upper()
-k=int(input("Ingrese la llave:_"))
-codigo=[]
-cifrado=[]
+
+frase = input("Ingrese la frase a cifrar:_")
+frase = frase.upper()
+k = int(input("Ingrese la llave:_"))
+codigo = []
+cifrado = []
+
+# Convertir cada letra en su índice correspondiente
 for i in frase:
-    codigo.append(diccionario[i])
-j=0
-for i in codigo:
-    codigo[j]=(i+k)%len(A)
-    j+=1 
+    if i in diccionario:  # Si el carácter está en el diccionario, cifrarlo
+        codigo.append((diccionario[i] + k) % len(A))
+    else:
+        codigo.append(i)  # Si es un espacio u otro carácter, mantenerlo igual
 
+# Convertir los índices cifrados de nuevo a letras
 for i in codigo:
-    cifrado.append(A[i])
+    if isinstance(i, int):  # Si es un número, es una letra cifrada
+        cifrado.append(A[i])
+    else:
+        cifrado.append(i)  # Si es un espacio, mantenerlo igual
 
-print(f"El cifrado es: {cifrado}")
+print(f"El cifrado es: {''.join(cifrado)}")
